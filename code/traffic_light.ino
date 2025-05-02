9189e62e2e3b34a1e59ce55208c2dc20e38ccbe6
Hobitronics <hobitronicsin@gmail.com>
2025-05-02 11:13:39 +0530

    Create traffic_light.ino

---
+++ code/traffic_light.ino
@@ -0,0 +1,26 @@
+int red = 8;
+int yellow = 9;
+int green = 10;
+void setup() {
+pinMode(red, OUTPUT);
+pinMode(yellow, OUTPUT);
+pinMode(green, OUTPUT);
+}
+void loop() {
+// RED ON, others OFF
+digitalWrite(red, HIGH);
+digitalWrite(yellow, LOW);
+digitalWrite(green, LOW);
+delay(3000); // Red for 3 seconds
+
+// YELLOW ON, others OFF
+digitalWrite(red, LOW);
+digitalWrite(yellow, HIGH);
+digitalWrite(green, LOW);
+delay(1000); // Yellow for 1 second
+// GREEN ON, others OFF
+digitalWrite(red, LOW);
+digitalWrite(yellow, LOW);
+digitalWrite(green, HIGH);
+delay(3000); // Green for 3 seconds
+}
